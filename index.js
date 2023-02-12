1be3354319b8ca54bfd78b99415cbd4485aee710
StephOge <nwachukwustephenie0@gmail.com>
2023-02-13 00:10:16 +0100

    javaScript basics

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log('stutern cohort 1.4');
+
+function multiply (number1, number2) {
+    return number1 * number2;
+}
+console.log (multiply(56, 2));
+
+
+//create variables
+let firstName = "Stephenie";
+let lastName = "Nwachukwu";
+let stuternTrack = "Frontend_developer";
+let skills = "Html, CSS, JavaScript, React, Ui/Ux";
+let favouriteColor = "blue";
+let yearOfCountryIndependence = "1990";
+
+console.log("My name is " + firstName +  lastName +"."+  " I am a " + stuternTrack +"." + 
+" My skills are " + skills + "." + " My favourite color is " + favouriteColor + "." 
++ " I am a Nigerian, My country found its independence in the year " + yearOfCountryIndependence + ".")
